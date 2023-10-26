10c7ce78b5621c512b521922e14732a68372b4a8
Akshit Agrawal <me@akshitagrawal.dev>
2023-10-26 17:07:19 +0530

    dockerfile changes

--- discord-ping.js
+++ discord-ping.js
@@ -1,11 +1,11 @@
 const { default: axios } = require("axios");
 
 const webhook =
-  process.env.PLGUIN_WEBHOOK_URL ||
+  process.env.PLUGIN_WEBHOOK_URL ||
   "https://discord.com/api/webhooks/1166978146793426944/UPlu4v1zgGHqA4DAt3YCpLsXzQuQFbImARjhZR48N2FnyERdZn6J5UQmRTIO_mpe6dsn";
 
 axios.post(webhook, {
-  content: process.env.PLGUIN_MESSAGE || "Hello, World!",
+  content: process.env.PLUGIN_MESSAGE || "Hello, World!",
   username: "Harness CI Bot",
   avatar_url:
     "https://images.crunchbase.com/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/vpfksuwzidzroxuk4llw",
